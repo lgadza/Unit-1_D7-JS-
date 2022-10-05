43714af409dfb0485181ee62b5118cdb3aff9c98
Louis Gadza <siuolgadza@gmail.com>
2022-10-05 16:09:21 +0200

    Unit 1_D7(JS)

---
+++ HomeWorkExtra/homeWorkExtra.js
@@ -0,0 +1,136 @@
+// Additional assignments for Day 3
+
+/*EX 1
+ Use a ternary operator to assign to a variable called gender the string values "male" or "female".
+ The choice should be made based on the value of another variable called isMale.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+let gender = "male";
+let isMale = gender === "male" ? console.log(true) : console.log(false);
+console.log(isMale);
+
+/*EX 2
+ Write a piece of code for checking if, given two integers, the value of one of them is 8 or if their addition or subtraction is equal to 8.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+let num1; // = 0;
+let num2; // = 0;
+if (num1 === 8 && num2 === 8) {
+  console.log(`The value of both num1 and num2 is 8 `);
+} else if (num1 === 8) {
+  console.log(`The value of num1  is 8 `);
+} else if (num2 === 8) {
+  console.log(`The value of num2  is 8 `);
+} else if (num1 + num2 === 8) {
+  console.log(`The sum of num1 and num2 is equal to 8`);
+} else if (num1 - num2 === 8 || num2 - num1 === 8) {
+  console.log(`The subtraction of the two integers is equal to 8`);
+} else {
+  console.log("Non of num1 or num2 is equal to 8 and,");
+  console.log(
+    "The addition or subtraction  of num1 and num2 is not equal to 8"
+  );
+}
+
+/*EX 3
+ Create a variable and assign to it the concatenation of two strings.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+let str1; //= "Lou";
+let str2; //= "is";
+let myName = str1 + str2;
+//console.log(myName);
+
+/*EX 4
+ Create three variables and assign a numerical value to each one of them. 
+ Using a conditional statement, write a piece of code for sorting their values from highest to lowest.
+ Display the result in the console.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+let x; //= 0;
+let y; //= 0;
+let z; //= 7;
+if (x === y && x === z) {
+  console.log(`All the values are equal`);
+} else if (x >= y && x >= z) {
+  if (y >= z) {
+    console.log(`x, y, z`);
+  } else {
+    console.log(`x, z, y`);
+  }
+} else if (y >= x && y >= z) {
+  if (x >= z) {
+    console.log(`y, x, z`);
+  } else {
+    console.log(`y, z, x`);
+  }
+} else if (z >= y && z >= x) {
+  if (y >= x) {
+    console.log(`z, y, x`);
+  } else {
+    console.log(`z, x, y`);
+  }
+}
+/*EX 5
+ Write a piece of code for finding the average of two given integers.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+let int1; // = 10;
+let int2; // = 2;
+let average = (int1 + int2) / 2;
+//console.log(average);
+
+/*EX 6
+ Write a piece of code for finding the longest of two given strings.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+let string1 = "Louis Gadza";
+let string2 = "Nombulelo Ndebele";
+if (string1.length > string2.length) {
+  console.log(`string1 is longer than string2`);
+} else if (string1.length === string2.length) {
+  console.log(`Both strings have the same length`);
+} else {
+  console.log(`string2 in longer that string1`);
+}
+
+/*EX 7
+ Write a piece of code for checking if a given value is a integer or not.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+let isInt; // = "8";
+if (typeof isInt === "number") {
+  console.log(true);
+} else {
+  console.log(`The value is not an integer`);
+}
+/*EX 8
+ Write a piece of code for calculating a certain percentage of a given number.
+ (Ex.: the 20% of 400 is 80)
+*/
+
+/* WRITE YOUR ANSWER HERE */
+let number; //= 15;
+let percent; //= 50;
+let percentageOfNumber = (percent / 100) * number;
+
+//console.log(percentageOfNumber);
+
+/*EX 9
+ Write a piece of code for checking if a given number is even or odd.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+let num; //= 7;
+if (num % 2 === 0) {
+  console.log(`num is even`);
+} else {
+  console.log(`num is old`);
+}
---
+++ app.js
@@ -0,0 +1,124 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be available for the tutors by the end of the day (5PM CET)
+*/
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+
+/* WRITE YOUR ANSWER HERE 
+
+a)Strings --> this is any sequence of characters inclosed in a single or double cots eg "LOuis", "24"
+
+b) numbers --> it is a numerical value, e.g 2, 5 but "5" is not a number because it is enclosed in cots. numbers can either be float or an integer
+c) Boolean --> this is true or false, these are the only two possible values for a boolean. if true or false are put in cots then they instantly become strings.
+
+*/
+
+/* EXERCISE 2
+ Try to describe what a variable is, in your own words
+ 
+*/
+
+/* WRITE YOUR ANSWER HERE 
+
+a variable is any thing that is declared to hold the data, it is like a container that can carry any inputs put in it..
+
+*/
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+//WRITE YOUR ANSWER HERE
+
+let num1 = 12;
+let num2 = 20;
+
+let sum = num1 + num2;
+console.log(sum);
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+let x = 12;
+
+/* EXERCISE 5
+ Create a variable called name and assign to it the string "John Doe".
+*/
+
+/* WRITE YOUR ANSWER HERE*/
+let name = "John Doe";
+
+/* EXERCISE 6
+ Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
+*/
+
+/* WRITE YOUR ANSWER HERE*/
+
+let result = 12 - x;
+console.log(result);
+
+/* EXERCISE 7
+ Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
+*/
+
+/* WRITE YOUR ANSWER HERE*/
+
+let name1 = "john";
+let name2 = "John";
+if (name1 !== name2) {
+  console.log(`name1 is different from name2`);
+  console.log(true);
+}
+
+// if (name1.toLowerCase() === name2.toLowerCase()) {
+//   console.log(`name1 is the same as name2 when both are lowercase`);
+// }
+if (name1 == name2) {
+  console.log(`${name1} is the same as ${name2} when both are lowercase`);
+}
+
+//TODO
+/* EXERCISE 8
+ Create a variable named x, and assign to it a numeric value less than 10.
+ Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
+*/
+
+/* WRITE YOUR ANSWER HERE*/
+
+// let x = 3;
+// if (x === 3) {
+//   console.log("three");
+// }
+
+/* EXERCISE 9
+ [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
+ try to search it by yourself, tomorrow morning we'll discuss it together!)
+*/
+
+/* WRITE YOUR ANSWER HERE*/
+let temp = 25;
+temperature =
+  temp >= 35
+    ? console.log(`Do not go outside, it is hot`)
+    : temp > 15 && temp < 35
+    ? console.log(
+        `You can go and enjoy yourself outside, the weather is great!`
+      )
+    : temp < 0
+    ? console.log(`Do not go outside, it is freezing`)
+    : console.log("Stay indoors");
+
+console.log(temperature);
